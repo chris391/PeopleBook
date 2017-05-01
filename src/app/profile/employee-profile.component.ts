5306ca5b05972b25ee0aab7225512facae6d2053
marcosuarez <marco_A_suarez_l@yahoo.com>
2017-05-01 18:23:59 +0200

    Merge remote-tracking branch 'origin/PB-0' into PB-1
    # Conflicts:
    #	src/app/add/add-employee.component.ts
    #	src/app/app.component.css
    #	src/app/app.component.html
    #	src/app/home/home.component.css
    #	src/app/home/home.component.ts
    #	src/server/app.js
    #	src/server/employee.model.js

---
+++ src/app/profile/employee-profile.component.ts
@@ -0,0 +1,71 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {SharedService} from "../shared/service/shared-service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {DataService} from "../services/data.service";
+import {ToastComponent} from "../shared/toast/toast.component";
+import {HomeComponent} from "../home/home.component";
+
+@Component({
+  selector: 'employee-profile',
+  templateUrl: './employee-profile.component.html',
+  styleUrls: ['./employee-profile.component.css']
+})
+export class EmployeeProfileComponent implements OnInit {
+  //@Input() ;
+  employee : any;
+  objectID : any;
+  isLoading = true;
+
+
+  constructor(private sharedService : SharedService, private route : ActivatedRoute, private dataService: DataService, private router: Router,
+    private toast: ToastComponent, private homeComponent : HomeComponent) {
+
+  }
+
+
+  ngOnInit() {
+
+    this.route.params.subscribe(param=>{
+      let id = param['id'];
+      this.objectID = id;
+    })
+
+    this.dataService.getEmployee(this.objectID).subscribe(employee=>{
+      this.employee = employee;
+    },
+      error=> console.log(error),
+      ()=> this.isLoading = false
+    )
+
+
+  }
+  gotoEdit(){
+    let link = ['/edit', this.objectID];
+    this.router.navigate(link);
+
+  }
+  deleteEmployee(){
+    if (window.confirm('Are you sure you want to permanently delete this item?')) {
+      this.dataService.deleteEmployee(this.employee).subscribe(
+        res => {
+        },
+        error => console.log(error),
+        () => {
+          this.toast.setMessage('item deleted successfully.', 'success');
+          let index = this.homeComponent.employees.indexOf(this.employee);
+          this.homeComponent.employees.splice(index, 1);
+          this.router.navigateByUrl('/home');
+
+        }
+      );
+    }
+  }
+
+}
+
+
+
+
+
+
+
